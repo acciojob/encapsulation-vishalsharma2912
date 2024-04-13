92f039df9d03ea465d9883847999539f36b3f2dc
Vishal <vishaljind04@gmail.com>
2024-04-13 15:38:07 +0530

    commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+
+
+        RWOnly obj = new RWOnly();
+        obj.setName("Vishal");
+        System.out.println(obj.getName());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+  private String name;
+  public void setName(String str){
+      name=str;
+  }
+   public String getName(){
+      return name;
+   }
+
+
 }
